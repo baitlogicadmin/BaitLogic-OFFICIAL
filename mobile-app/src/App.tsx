620611a4d1df0d4cbbbbe4842f27a3475c8b4923
BaitLogic Outdoors <baitlogicadmin@gmail.com>
2026-08-22 07:17:09 -0500

    feat: mount Water Watch conservation intelligence

--- mobile-app/src/App.tsx
+++ mobile-app/src/App.tsx
@@ -1,11 +1,13 @@
 import Prototype from "./Prototype";
 import FeatureTools from "./FeatureTools";
+import WaterWatch from "./WaterWatch";
 
 export default function App() {
   return (
     <>
       <FeatureTools />
       <Prototype />
+      <WaterWatch />
     </>
   );
 }
