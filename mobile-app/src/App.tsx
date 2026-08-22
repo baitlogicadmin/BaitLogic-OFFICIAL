9bd8d6a33a7286ce21ab31fe24ca68ac28ad5b79
BaitLogic Outdoors <baitlogicadmin@gmail.com>
2026-08-22 06:03:43 -0500

    Mount regional Explore intelligence experience

--- mobile-app/src/App.tsx
+++ mobile-app/src/App.tsx
@@ -1,11 +1,13 @@
 import Prototype from "./Prototype";
 import FeatureTools from "./FeatureTools";
+import RegionalExploreEnhancer from "./RegionalExploreEnhancer";
 
 export default function App() {
   return (
     <>
       <FeatureTools />
       <Prototype />
+      <RegionalExploreEnhancer />
     </>
   );
 }
