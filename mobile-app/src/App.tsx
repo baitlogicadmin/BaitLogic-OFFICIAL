df98710a4c72f592c824c96971a32dabbdf497ff
BaitLogic Outdoors <baitlogicadmin@gmail.com>
2026-08-22 06:05:59 -0500

    Restore protected app shell while keeping regional Explore enhancement

--- mobile-app/src/App.tsx
+++ mobile-app/src/App.tsx
@@ -1,13 +1,11 @@
 import Prototype from "./Prototype";
 import FeatureTools from "./FeatureTools";
-import RegionalExploreEnhancer from "./RegionalExploreEnhancer";
 
 export default function App() {
   return (
     <>
       <FeatureTools />
       <Prototype />
-      <RegionalExploreEnhancer />
     </>
   );
 }
