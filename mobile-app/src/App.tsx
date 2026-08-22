efa7b2587c025481490a15fdbf1b8823cdb0db8b
BaitLogic Outdoors <baitlogicadmin@gmail.com>
2026-08-22 07:19:11 -0500

    fix: preserve protected mobile runtime App shell

--- mobile-app/src/App.tsx
+++ mobile-app/src/App.tsx
@@ -1,13 +1,11 @@
 import Prototype from "./Prototype";
 import FeatureTools from "./FeatureTools";
-import WaterWatch from "./WaterWatch";
 
 export default function App() {
   return (
     <>
       <FeatureTools />
       <Prototype />
-      <WaterWatch />
     </>
   );
 }
