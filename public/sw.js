18c016072c408f7f3a4b1d3fb174efaa66321953
BaitLogic Outdoors <baitlogicadmin@gmail.com>
2026-08-23 07:43:19 -0500

    Cache approved BaitLogic visuals for offline use

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const CACHE_NAME="baitlogic-offline-v16";
-const DATA_CACHE="baitlogic-data-v16";
+const CACHE_NAME="baitlogic-offline-v17";
+const DATA_CACHE="baitlogic-data-v17";
 const QUEUE_DB="baitlogic-offline-queue-v1";
 const QUEUE_STORE="requests";
 
@@ -12,6 +12,7 @@ const APP_SHELL=[
   "/barometer.html",
   "/nature-check.html",
   "/conservation-prairie.html",
+  "/approved-visual.css",
   "/site.css?v=1",
   "/site.css?v=2",
   "/site.css?v=3",
