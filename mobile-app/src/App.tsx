e865dc22a2f6e498c58a4b5fdcee7d53ac859610
BaitLogic Outdoors <baitlogicadmin@gmail.com>
2026-08-31 15:55:52 -0500

    feat: render mobile and desktop as separate implementations

--- mobile-app/src/App.tsx
+++ mobile-app/src/App.tsx
@@ -1,11 +1,21 @@
-import Prototype from "./Prototype";
+import { useEffect, useState } from "react";
 import FeatureTools from "./FeatureTools";
+import MobileDashboard from "./MobileDashboard";
+import DesktopDashboard from "./DesktopDashboard";
 
-export default function App() {
-  return (
-    <>
-      <FeatureTools />
-      <Prototype />
-    </>
-  );
+function useDesktopMode(){
+  const query="(min-width: 761px)";
+  const [desktop,setDesktop]=useState(()=>window.matchMedia(query).matches);
+  useEffect(()=>{
+    const media=window.matchMedia(query);
+    const onChange=()=>setDesktop(media.matches);
+    media.addEventListener("change",onChange);
+    return()=>media.removeEventListener("change",onChange);
+  },[]);
+  return desktop;
+}
+
+export default function App(){
+  const desktop=useDesktopMode();
+  return <><FeatureTools />{desktop?<DesktopDashboard/>:<MobileDashboard/>}</>;
 }
